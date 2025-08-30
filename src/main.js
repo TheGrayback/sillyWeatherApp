0a0b0fba0154a26634dafae83ef5fe5301da3525
TheGrayback <nchmil18@gmail.com>
2025-08-30 16:49:21 +0300

    API connect

--- src/main.js
+++ src/main.js
@@ -1,5 +1,48 @@
 import axios from 'axios';
 
-
-
 lucide.createIcons();
+
+const cityInput = document.getElementById('city-input');
+
+cityInput.addEventListener('keydown', async (event) => {
+    if (event.key === 'Enter') {
+        const city = cityInput.value.trim();
+        const coords = await GetCoordinates(city);
+        const weather = await GetWeather(coords.latitude, coords.longitude);
+        SetTemp(weather.current.temperature_2m);
+    }
+});
+
+async function GetCoordinates(city) {
+    const response = await axios.get(
+        'https://geocoding-api.open-meteo.com/v1/search',
+        {
+            params: {
+                name: city,
+                count: 1,
+                language: 'en',
+                format: 'json',
+            },
+        }
+    );
+    const { admin1, country, name, latitude, longitude } =
+        response.data.results[0];
+    return { admin1, country, name, latitude, longitude };
+}
+
+async function GetWeather(lat, lon) {
+    const response = await axios.get('https://api.open-meteo.com/v1/forecast', {
+        params: {
+            latitude: lat,
+            longitude: lon,
+            current: 'temperature_2m',
+        },
+    });
+    console.log(response.data);
+    return response.data;
+}
+
+function SetTemp(temp) {
+    const tempValue = document.getElementById('temperature-value');
+    tempValue.innerHTML = `${temp}<sup>&deg;c</sup>`;
+}
