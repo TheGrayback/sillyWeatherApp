9222b535a5fbe3aad4dc6a97b8ba31d34a9f9491
TheGrayback <nchmil18@gmail.com>
2025-09-14 22:34:52 +0300

    feat(ui): create api adapter to reduce amount of function parameters

---
+++ src/apiAdapter.js
@@ -0,0 +1,20 @@
+export function mapWeatherApiResponse(weatherResponse, coordinates) {
+    return {
+        current: {
+            temp: weatherResponse.current.temperature_2m,
+            unit: weatherResponse.current_units.temperature_2m,
+            code: weatherResponse.current.weather_code,
+            time: weatherResponse.current.time,
+        },
+        location: {
+            city: coordinates.name,
+            country: coordinates.country_code,
+        },
+        forecast: weatherResponse.daily.time.map((date, index) => ({
+            date,
+            tempMax: weatherResponse.daily.temperature_2m_max[index],
+            tempMin: weatherResponse.daily.temperature_2m_min[index],
+            code: weatherResponse.daily.weather_code[index],
+        })),
+    };
+}
