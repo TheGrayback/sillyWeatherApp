5f0531493beca04e858d353fa016ad5aeffda41c
TheGrayback <nchmil18@gmail.com>
2025-09-14 22:35:41 +0300

    refactor(ui): restructure UI-related functions after API adapter

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { getCoordinates, getWeather } from './api';
-import { setForecast, setGeolocation, setTemp, setTime, setWeatherCondition } from './ui';
+import { mapWeatherApiResponse } from './apiAdapter';
+import { setCurrentWeather, setForecast } from './ui';
 
 lucide.createIcons();
 
@@ -9,14 +10,12 @@ cityInput.addEventListener('keydown', async (event) => {
     if (event.key === 'Enter') {
         const city = cityInput.value.trim();
         const coordinates = await getCoordinates(city);
-        const weather = await getWeather(coordinates.latitude, coordinates.longitude);
-        setTemp(
-            weather.current.temperature_2m,
-            weather.current_units.temperature_2m
+        const weather = await getWeather(
+            coordinates.latitude,
+            coordinates.longitude
         );
-        setWeatherCondition(weather.current.weather_code);
-        setGeolocation(coordinates.name, coordinates.country_code);
-        setTime(weather.current.time);
-        setForecast(weather.daily.time, weather.daily.temperature_2m_max, weather.daily.temperature_2m_min, weather.daily.weather_code);
+        const weatherData = mapWeatherApiResponse(weather, coordinates);
+        setCurrentWeather(weatherData);
+        setForecast(weatherData);
     }
 });
--- src/ui.js
+++ src/ui.js
@@ -2,13 +2,20 @@ import { weatherCodeDescription } from './utils/weatherCodes';
 
 const BASE_URL = import.meta.env.BASE_URL;
 
-export function setTemp(temperatureValue, temperatureUnit) {
+export function setCurrentWeather({current, location}) {
+    setWeatherCondition(current.code);
+    setTemp(current.temp, current.unit);
+    setGeolocation(location.city, location.country);
+    setTime(current.time);
+}
+
+function setTemp(temperatureValue, temperatureUnit) {
     const tempValue = document.getElementById('temperature-value');
     tempValue.innerHTML =
         `${temperatureValue}<sup>${temperatureUnit}</sup>` || '---';
 }
 
-export function setWeatherCondition(code) {
+function setWeatherCondition(code) {
     const weatherDesc = document.getElementById(
         'weather-condition-description'
     );
@@ -26,35 +33,41 @@ export function setWeatherCondition(code) {
     lucide.createIcons();
 }
 
-export function setGeolocation(city, country_code) {
+function setGeolocation(city, country_code) {
     const userGeolocation = document.getElementById('user-geolocation');
     userGeolocation.textContent = `${city}, ${country_code}`;
 }
 
-export function setTime(isoTime) {
+function setTime(isoTime) {
+    console.log(isoTime);
     const userTime = document.getElementById('user-time');
-    const date = new Date(isoTime);
+    const date = new Date(`${isoTime}`);
     const dateOptions = {
         day: 'numeric',
         month: 'long',
         year: 'numeric',
     };
     const timeOptions = {
-        hours: '2-digit',
-        minutes: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
     };
     const dateFormatted = _formatTime('en-Gb', dateOptions, date);
     const timeFormatted = _formatTime('en-Gb', timeOptions, date);
+    const testTimeFormatted = new Intl.DateTimeFormat(
+        'en-GB',
+        timeOptions
+    ).format(date)
+    console.log(timeFormatted, dateFormatted, testTimeFormatted);
     userTime.innerHTML =
         `${dateFormatted}<span class="font-bold">, ${timeFormatted}</span>` ||
         '---';
 }
 
-export function setForecast(forecastDate, forecastTempMax, forecastTempMin, weatherCode) {
+export function setForecast({forecast}) {
     const day = document.querySelectorAll('[day-data]');
     day.forEach((forecastDay, dayIndex) => {
         const dataIndex = dayIndex + 1;
-        const date = new Date(forecastDate[dataIndex]);
+        const date = new Date(forecast[dataIndex].date);
         const dateOptions = {
             day: 'numeric',
             month: 'long',
@@ -64,19 +77,20 @@ export function setForecast(forecastDate, forecastTempMax, forecastTempMin, weat
         };
         const dateFormatted = _formatTime('en-Gb', dateOptions, date);
         const weekdayFormatted = _formatTime('en-Gb', weekdayOptions, date);
-        forecastDay.querySelector('.forecast-icon').src = `${BASE_URL}${weatherCodeDescription[weatherCode[dataIndex]].day.largeIcon}`
+        forecastDay.querySelector('.forecast-icon').src =
+            `${BASE_URL}${weatherCodeDescription[forecast[dataIndex].code].day.largeIcon}`;
         forecastDay.querySelector('.forecast-day-month').textContent =
             dateFormatted;
         forecastDay.querySelector('.forecast-weekday').textContent =
             weekdayFormatted;
         forecastDay.querySelector('.forecast-temp-max').innerHTML =
-            `${Math.round(forecastTempMax[dataIndex])}<sup>&deg;c</sup>`;
+            `${Math.round(forecast[dataIndex].tempMax)}<sup>&deg;c</sup>`;
         forecastDay.querySelector('.forecast-temp-min').innerHTML =
-            `${Math.round(forecastTempMin[dataIndex])}<sup>&deg;c</sup>`;
+            `${Math.round(forecast[dataIndex].tempMin)}<sup>&deg;c</sup>`;
     });
 }
 
 // Silly useless function but it itched my brain
-function _formatTime (locale = 'en-GB', options, baseDate) {
-    return new Intl.DateTimeFormat(locale, options).format(baseDate)
+function _formatTime(locale = 'en-GB', options, baseDate) {
+    return new Intl.DateTimeFormat(locale, options).format(baseDate);
 }
